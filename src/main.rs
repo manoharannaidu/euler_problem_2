5e9d555c76f77121948e2b87609ae7beca9ae016
manonaid <manoharan.naidu@gmail.com>
2023-11-16 22:40:26 +0530

    tried to retouch the solution to avoid assigning unnecessary memomry

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ fn main() {
    let mut second: u32 = 2;
    let mut even_fib_sum: u32 = 2;
    loop {
-      let next_fib = first + second;
-      if next_fib % 2 == 0 {
-         even_fib_sum = even_fib_sum + next_fib;
-      }
-      first = second;
-      second = next_fib;
-      if next_fib > 4000000 {
+      if (first + second) > 4000000 {
          break;
       }
+      if (first + second)% 2 == 0 {
+         even_fib_sum = even_fib_sum + (first + second);
+      }
+      let _next_fib = &first + &second;
+      first = second;
+      second = _next_fib;
    }
-   println!("{even_fib_sum}")
+   println!("{even_fib_sum}");
 }
